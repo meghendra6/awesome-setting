8b42067e21df987357162c6764ea2473efbe8b6f
Scott Lee <ji6.lee@samsung.com>
2020-01-24 09:53:04 +0900

    Fix hotkey settings

--- rc.lua
+++ rc.lua
@@ -274,19 +274,19 @@ globalkeys = my_table.join(
               {description = "go back", group = "tag"}),
 
     -- Non-empty tag browsing
-    awful.key({ altkey }, "Left", function () lain.util.tag_view_nonempty(-1) end,
-              {description = "view  previous nonempty", group = "tag"}),
-    awful.key({ altkey }, "Right", function () lain.util.tag_view_nonempty(1) end,
-              {description = "view  previous nonempty", group = "tag"}),
+--    awful.key({ altkey }, "Left", function () lain.util.tag_view_nonempty(-1) end,
+--              {description = "view  previous nonempty", group = "tag"}),
+--    awful.key({ altkey }, "Right", function () lain.util.tag_view_nonempty(1) end,
+--              {description = "view  previous nonempty", group = "tag"}),
 
     -- Default client focus
-    awful.key({ altkey,           }, "j",
+    awful.key({ modkey,           }, "j",
         function ()
             awful.client.focus.byidx( 1)
         end,
         {description = "focus next by index", group = "client"}
     ),
-    awful.key({ altkey,           }, "k",
+    awful.key({ modkey,           }, "k",
         function ()
             awful.client.focus.byidx(-1)
         end,
@@ -294,32 +294,32 @@ globalkeys = my_table.join(
     ),
 
     -- By direction client focus
-    awful.key({ modkey }, "j",
-        function()
-            awful.client.focus.global_bydirection("down")
-            if client.focus then client.focus:raise() end
-        end,
-        {description = "focus down", group = "client"}),
-    awful.key({ modkey }, "k",
-        function()
-            awful.client.focus.global_bydirection("up")
-            if client.focus then client.focus:raise() end
-        end,
-        {description = "focus up", group = "client"}),
-    awful.key({ modkey }, "h",
-        function()
-            awful.client.focus.global_bydirection("left")
-            if client.focus then client.focus:raise() end
-        end,
-        {description = "focus left", group = "client"}),
-    awful.key({ modkey }, "l",
-        function()
-            awful.client.focus.global_bydirection("right")
-            if client.focus then client.focus:raise() end
-        end,
-        {description = "focus right", group = "client"}),
-    awful.key({ modkey,           }, "w", function () awful.util.mymainmenu:show() end,
-              {description = "show main menu", group = "awesome"}),
+--    awful.key({ modkey }, "j",
+--        function()
+--            awful.client.focus.global_bydirection("down")
+--            if client.focus then client.focus:raise() end
+--        end,
+--        {description = "focus down", group = "client"}),
+--    awful.key({ modkey }, "k",
+--        function()
+--            awful.client.focus.global_bydirection("up")
+--            if client.focus then client.focus:raise() end
+--        end,
+--        {description = "focus up", group = "client"}),
+--    awful.key({ modkey }, "h",
+--        function()
+--            awful.client.focus.global_bydirection("left")
+--            if client.focus then client.focus:raise() end
+--        end,
+--        {description = "focus left", group = "client"}),
+--    awful.key({ modkey }, "l",
+--        function()
+--            awful.client.focus.global_bydirection("right")
+--            if client.focus then client.focus:raise() end
+--        end,
+--        {description = "focus right", group = "client"}),
+--    awful.key({ modkey,           }, "w", function () awful.util.mymainmenu:show() end,
+--              {description = "show main menu", group = "awesome"}),
 
     -- Layout manipulation
     awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
